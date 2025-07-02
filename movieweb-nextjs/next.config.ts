b5c75bd87075bd641ce29d6d5982d653ff8ef852
binhnguyen1709@gmail.com <ngolinh09032002@gmail.com>
2025-07-02 10:07:21 +0700

    up

--- movieweb-nextjs/next.config.ts
+++ movieweb-nextjs/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: "export",
+    assetPrefix: isProd ? "/ten-repo-cua-ban/" : "",
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
